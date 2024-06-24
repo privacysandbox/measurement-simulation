83d5ae11dcbd547b7344d32751d3f2dfbfdbdb19
Yaw Frempong <yawfrempong@google.com>
2024-06-24 22:10:33 +0000

    Sync GitHub Repo with Internal Codebase (06-24-2024)

--- header-validation/src/index.js
+++ header-validation/src/index.js
@@ -1,82 +1,122 @@
-const {validateSource} = require('./validate_source')
-const {validateTrigger} = require('./validate_trigger')
+const {validateSource} = require('./validate_source');
+const {validateTrigger} = require('./validate_trigger');
 
 // Initialize page elements
-const validationForm = document.getElementById('validation-form')
-const inputTextbox = document.getElementById('input-text')
-const headerOptions = validationForm.elements.namedItem('header')
-const sourceTypeOptionsContainer = document.getElementById('source-type-options')
-const sourceTypeOptions = validationForm.elements.namedItem('source-type')
-const registrationOptions = validationForm.elements.namedItem('registration-type')
-const featureAraParsingAlignmentV1 = validationForm.elements.namedItem('feature-ara-parsing-alignment-v1')
-const featureLookbackWindowFilter = validationForm.elements.namedItem('feature-lookback-window-filter')
-const featureTriggerDataMatching = validationForm.elements.namedItem('feature-trigger-data-matching')
-const copyButton = document.getElementById('linkify')
-const errorList = document.getElementById('errors')
-const warningList = document.getElementById('warnings')
-const noteList = document.getElementById('notes')
-const successDiv = document.getElementById('success')
+const validationForm = document.getElementById('validation-form');
+const inputTextbox = document.getElementById('input-text');
+const headerOptions = validationForm.elements.namedItem('header');
+const featureLookbackWindowFilter = validationForm.elements.namedItem('feature-lookback-window-filter');
+const featureTriggerDataMatching = validationForm.elements.namedItem('feature-trigger-data-matching');
+const featureCoarseEventReportDestinations = validationForm.elements.namedItem('feature-coarse-event-report-destination');
+const featureSharedSourceDebugKey = validationForm.elements.namedItem('feature-shared-source-debug-key');
+const featureXNA = validationForm.elements.namedItem('feature-xna');
+const featureSharedFilterDataKeys = validationForm.elements.namedItem('feature-shared-filter-data-keys');
+const featurePreinstallCheck = validationForm.elements.namedItem('feature-preinstall-check');
+const featureAttributionScopes = validationForm.elements.namedItem('feature-attribution-scopes');
+const featureAggregationCoordinatorOrigin = validationForm.elements.namedItem('feature-aggregation-coordinator-origin');
+const featureSourceRegistrationTimeOptionalForAggReports = validationForm.elements.namedItem('feature-source-registration-time-optional-for-agg-reports');
+const featureTriggerContextId = validationForm.elements.namedItem('feature-trigger-context-id');
+const featureEnableUpdateTriggerHeaderLimit = validationForm.elements.namedItem('feature-enable-update-trigger-header-limit');
+const featureEnableReinstallReattribution = validationForm.elements.namedItem('feature-enable-reinstall-reattribution');
+const copyButton = document.getElementById('linkify');
+const errorList = document.getElementById('errors');
+const warningList = document.getElementById('warnings');
+const noteList = document.getElementById('notes');
+const successDiv = document.getElementById('success');
 
 // Add listeners
-validationForm.addEventListener('input', validate)
-copyButton.addEventListener('click', copyLink)
+validationForm.addEventListener('input', validate);
+copyButton.addEventListener('click', copyLink);
 
 
 // Define functions
 async function copyLink() {
-  const url = new URL(location.toString())
-  url.search = ''
-  url.searchParams.set('header', headerOptions.value)
-  url.searchParams.set('json', inputTextbox.value)
-  if (url.searchParams.get('header') === 'source') {
-    url.searchParams.set('source-type', sourceTypeOptions.value)
-  }
-  url.searchParams.set('feature-ara-parsing-alignment-v1', featureAraParsingAlignmentV1.checked.toString())
-  url.searchParams.set('feature-lookback-window-filter', featureLookbackWindowFilter.checked.toString())
-  url.searchParams.set('feature-trigger-data-matching', featureTriggerDataMatching.checked.toString())
-  await navigator.clipboard.writeText(url.toString())
+  const url = new URL(location.toString());
+  url.search = '';
+  url.searchParams.set('header', headerOptions.value);
+  url.searchParams.set('json', inputTextbox.value);
+  url.searchParams.set('feature-lookback-window-filter', featureLookbackWindowFilter.checked.toString());
+  url.searchParams.set('feature-trigger-data-matching', featureTriggerDataMatching.checked.toString());
+  url.searchParams.set('feature-coarse-event-report-destination', featureCoarseEventReportDestinations.checked.toString());
+  url.searchParams.set('feature-shared-source-debug-key', featureSharedSourceDebugKey.checked.toString());
+  url.searchParams.set('feature-xna', featureXNA.checked.toString());
+  url.searchParams.set('feature-shared-filter-data-keys', featureSharedFilterDataKeys.checked.toString());
+  url.searchParams.set('feature-preinstall-check', featurePreinstallCheck.checked.toString());
+  url.searchParams.set('feature-attribution-scopes', featureAttributionScopes.checked.toString());
+  url.searchParams.set('feature-aggregation-coordinator-origin', featureAggregationCoordinatorOrigin.checked.toString());
+  url.searchParams.set('feature-source-registration-time-optional-for-agg-reports', featureSourceRegistrationTimeOptionalForAggReports.checked.toString());
+  url.searchParams.set('feature-trigger-context-id', featureTriggerContextId.checked.toString());
+  url.searchParams.set('feature-enable-update-trigger-header-limit', featureEnableUpdateTriggerHeaderLimit.checked.toString());
+  url.searchParams.set('feature-enable-reinstall-reattribution', featureEnableReinstallReattribution.checked.toString());
+  await navigator.clipboard.writeText(url.toString());
 }
 
 function populateUIList(element, items) {
-  element.replaceChildren([])
+  element.replaceChildren([]);
   items.forEach(function (item) {
-    let listItem = document.createElement('li')
+    let listItem = document.createElement('li');
     if (typeof item === "string") {
-      listItem.textContent = item
+      listItem.textContent = item;
     } else {
-      listItem.textContent = item.msg + ': ' + item.path
+      listItem.textContent = item.msg + ': ' + item.path;
     }
-    element.appendChild(listItem)
+    element.appendChild(listItem);
   })
 }
 
 function validate() {
-  // Enable/Disable source type option based on header type
-  sourceTypeOptionsContainer.disabled = (headerOptions.value != 'source') 
-
   // Fetch Flag Values
-  let flagValues = {}
-  flagValues['feature-ara-parsing-alignment-v1'] = featureAraParsingAlignmentV1.checked
-  flagValues['feature-lookback-window-filter'] = featureLookbackWindowFilter.checked
-  flagValues['feature-trigger-data-matching'] = featureTriggerDataMatching.checked
-  flagValues['max_attribution_filters'] = 50
-  flagValues['max_bytes_per_attribution_filter_string'] = 25
+  let flagValues = {};
+  flagValues['feature-lookback-window-filter'] = featureLookbackWindowFilter.checked;
+  flagValues['feature-trigger-data-matching'] = featureTriggerDataMatching.checked;
+  flagValues['feature-coarse-event-report-destination'] = featureCoarseEventReportDestinations.checked;
+  flagValues['feature-shared-source-debug-key'] = featureSharedSourceDebugKey.checked;
+  flagValues['feature-xna'] = featureXNA.checked;
+  flagValues['feature-shared-filter-data-keys'] = featureSharedFilterDataKeys.checked;
+  flagValues['feature-preinstall-check'] = featurePreinstallCheck.checked;
+  flagValues['feature-attribution-scopes'] = featureAttributionScopes.checked;
+  flagValues['feature-aggregation-coordinator-origin'] = featureAggregationCoordinatorOrigin.checked;
+  flagValues['feature-source-registration-time-optional-for-agg-reports'] = featureSourceRegistrationTimeOptionalForAggReports.checked;
+  flagValues['feature-trigger-context-id'] = featureTriggerContextId.checked;
+  flagValues['feature-enable-update-trigger-header-limit'] = featureEnableUpdateTriggerHeaderLimit.checked;
+  flagValues['feature-enable-reinstall-reattribution'] = featureEnableReinstallReattribution.checked;
+  flagValues['max_attribution_filters'] = 50;
+  flagValues['max_bytes_per_attribution_filter_string'] = 25;
   flagValues['max_values_per_attribution_filter'] = 50;
+  flagValues["max_distinct_web_destinations_in_source_registration"] = 3;
+  flagValues["max_web_destination_hostname_character_length"] = 253;
+  flagValues["max_web_destination_hostname_parts"] = 127;
+  flagValues["min_web_destination_hostname_part_character_length"] = 1;
+  flagValues["max_web_destination_hostname_part_character_length"] = 63;
+  flagValues["max_aggregate_keys_per_source_registration"] = 50;
+  flagValues["max_bytes_per_attribution_aggregate_key_id"] = 25;
+  flagValues["min_bytes_per_aggregate_value"] = 3;
+  flagValues["max_bytes_per_aggregate_value"] = 34;
+  flagValues["max_attribution_scopes_per_source"] = 20;
+  flagValues["max_attribution_scope_string_length"] = 50;
+  flagValues["max_report_states_per_source_registration"] = (1n << 32n) - 1n;
+  flagValues["max_trigger_context_id_string_length"] = 64;
+  flagValues["max_bucket_threshold"] = (1n << 32n) - 1n;
+  flagValues["max_filter_maps_per_filter_set"] = 5;
+  flagValues["max_aggregate_keys_per_trigger_registration"] = 50;
+  flagValues["max_sum_of_aggregate_values_per_source"] = 65536;
+  flagValues["max_aggregate_deduplication_keys_per_registration"] = 50;
+  flagValues['header_type'] = headerOptions.value;
 
   // Validate input
   let result;
-  if (sourceTypeOptionsContainer.disabled) {
-    result = validateTrigger(inputTextbox.value, registrationOptions.value, flagValues)
-  } else {
-    result = validateSource(inputTextbox.value, registrationOptions.value, flagValues)
+  if (headerOptions.value === 'source') {
+    result = validateSource(inputTextbox.value, flagValues);
+  } else if (headerOptions.value === 'trigger') {
+    result = validateTrigger(inputTextbox.value, flagValues);
   }
 
   // Show results
-  successDiv.textContent = (result.errors.length === 0 && result.warnings.length === 0) ? 'The header is valid.' : ''
-  populateUIList(errorList, result.errors)
-  populateUIList(warningList, result.warnings)
-  populateUIList(noteList, [])
+  successDiv.textContent = (result.errors.length === 0 && result.warnings.length === 0) ? 'The header is valid.' : '';
+  populateUIList(errorList, result.errors);
+  populateUIList(warningList, result.warnings);
+  populateUIList(noteList, []);
 }
 
 // Load initial state of page
-validate()
+validate();
--- header-validation/src/validate_source.js
+++ header-validation/src/validate_source.js
@@ -1,29 +1,29 @@
 const {
   State,
-  boolean,
   optBoolean,
   optBooleanFallback,
   uint64,
-  optUint64,
   int64,
-  optInt64,
+  string,
   optString,
   optStringFallback,
   object,
-  objectWithFlags,
   array,
-  required,
+  optArray,
   optional,
-  exclude,
-  getDataType,
   isValidAppDestinationHost,
-  isValidFilterData,
-  isValidTriggerMatchingData
+  isValidWebDestinationHost,
+  isValidSourceFilterData,
+  isValidTriggerMatchingData,
+  isValidAggregationKeys,
+  isValidAttributionScopes,
+  isValidAttributionScopeLimit,
+  isValidMaxEventStates
 } = require('./base');
 
-function validateSource(source, registrationType, flagValues) {
+function validateSource(source, flagValues) {
   try {
-    sourceJSON = JSON.parse(source)
+    sourceJSON = JSON.parse(source);
   } catch (err) {
     return {errors: [err instanceof Error ? err.toString() : 'unknown error'], warnings: []};
   }
@@ -32,51 +32,60 @@ function validateSource(source, registrationType, flagValues) {
   let jsonSpec = {};
 
   // destination check
-  if (!('destination' in sourceJSON) && !('web_destination' in sourceJSON)) {
+  if ((!('destination' in sourceJSON) || sourceJSON['destination'] === null) && (!('web_destination' in sourceJSON) || sourceJSON['web_destination'] === null)) {
     return {errors: [{
       path: ["destination or web_destination"],
-      msg: "At least one field must be present",
-      formattedError: "At least one field must be present: `destination or web_destination`"}], warnings: []};
+      msg: "At least one field must be present and non-null",
+      formattedError: "at least one field must be present and non-null: `destination or web_destination`"}], warnings: []};
   }
-  jsonSpec["destination"] = optional(isValidAppDestinationHost);
+
+  jsonSpec["destination"] = optional(optString(isValidAppDestinationHost));
   jsonSpec["web_destination"] = optional();
+  jsonSpec["source_event_id"] = optional(string(uint64()));
+  jsonSpec["expiry"] = optional(optString(uint64()));
+  jsonSpec["event_report_window"] = optional(optString(uint64()));
+  jsonSpec["aggregatable_report_window"] = optional(optString(uint64()));
+  jsonSpec["priority"] = optional(string(int64()));
+  jsonSpec["debug_key"] = optional(string(uint64()));
+  jsonSpec["debug_reporting"] = optional(optBooleanFallback());
+  jsonSpec["install_attribution_window"] = optional(optString(int64()));
+  jsonSpec["post_install_exclusivity_window"] = optional(optString(int64()));
+  jsonSpec["debug_ad_id"] = optional(optStringFallback());
+  jsonSpec["debug_join_key"] = optional(optStringFallback());
 
-  // define fields that are dependent on the feature-ara-parsing-alignment-v1 flag
-  if (flagValues['feature-ara-parsing-alignment-v1']) {
-    jsonSpec["source_event_id"] = optional(uint64);
-    jsonSpec["expiry"] = optional(optUint64);
-    jsonSpec["event_report_window"] = optional(optUint64);
-    jsonSpec["aggregatable_report_window"] = optional(optUint64);
-    jsonSpec["priority"] = optional(int64);
-    jsonSpec["debug_key"] = optional(uint64);
-  } else {
-    jsonSpec["source_event_id"] = optional(optUint64);
-    jsonSpec["expiry"] = optional(optInt64);
-    jsonSpec["event_report_window"] = optional(optInt64);
-    jsonSpec["aggregatable_report_window"] = optional(optInt64);
-    jsonSpec["priority"] = optional(optInt64);
-    jsonSpec["debug_key"] = optional(optUint64);
+  // flag dependent fields
+  jsonSpec["web_destination"] = optional(optArray(isValidWebDestinationHost, 'string', flagValues));
+  jsonSpec["filter_data"] = optional(object(isValidSourceFilterData, flagValues));
+  jsonSpec["aggregation_keys"] = optional(object(isValidAggregationKeys, flagValues));
+  if (flagValues['feature-attribution-scopes']) {
+    flagValues['max_event_states_present'] = ('max_event_states' in sourceJSON);
+    flagValues['attribution_scope_limit_present'] = ('attribution_scope_limit' in sourceJSON);
+    jsonSpec["attribution_scope_limit"] = optional(optString(int64(isValidAttributionScopeLimit, flagValues)));
+    jsonSpec["max_event_states"] = optional(optString(int64(isValidMaxEventStates, flagValues)));
+    jsonSpec["attribution_scopes"] = optional(array(isValidAttributionScopes, flagValues));
   }
 
-  // define fields that are dependent on the feature-trigger-data-matching flag
   if (flagValues['feature-trigger-data-matching']) {
-    jsonSpec["trigger_data_matching"] = optional(isValidTriggerMatchingData)
+    jsonSpec["trigger_data_matching"] = optional(optString(isValidTriggerMatchingData));
   }
-
-  // define fields that have no dependencies
-  jsonSpec["debug_reporting"] = optional(optBooleanFallback())
-  jsonSpec["install_attribution_window"] = optional(optInt64)
-  jsonSpec["post_install_exclusivity_window"] = optional(optInt64)
-  jsonSpec["debug_ad_id"] = optional(optStringFallback())
-  jsonSpec["debug_join_key"] = optional(optStringFallback())
-  jsonSpec["coarse_event_report_destinations"] = optional(optBoolean())
-  jsonSpec["shared_debug_keys"] = optional(optUint64);
-  jsonSpec["drop_source_if_installed"] = optional(optBoolean())
-  jsonSpec["shared_aggregation_keys"] = optional(array())
-  jsonSpec["shared_filter_data_keys"] = optional(array())
-
-  // define filter data spec
-  jsonSpec["filter_data"] = optional(objectWithFlags(isValidFilterData, flagValues))
+  if (flagValues['feature-coarse-event-report-destination']) {
+    jsonSpec["coarse_event_report_destinations"] = optional(optBoolean());
+  }
+  if (flagValues['feature-shared-source-debug-key']) {
+    jsonSpec["shared_debug_key"] = optional(optString(uint64()));
+  }
+  if (flagValues['feature-xna']) {
+    jsonSpec["shared_aggregation_keys"] = optional(array());
+  }
+  if (flagValues['feature-shared-filter-data-keys']) {
+    jsonSpec["shared_filter_data_keys"] = optional(array());
+  }
+  if (flagValues['feature-preinstall-check']) {
+    jsonSpec["drop_source_if_installed"] = optional(optBoolean());
+  }
+  if(flagValues['feature-enable-reinstall-reattribution']) {
+    jsonSpec["reinstall_reattribution_window"] = optional(optString(int64()));
+  }
 
   state.validate(sourceJSON, jsonSpec, flagValues);
   return state.result();
